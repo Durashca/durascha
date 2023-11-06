3ae7f2d0f54a1808eecff27e3026ae7e73263079
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-06 19:27:31 +0300

    69

---
+++ src/js/69.js
@@ -0,0 +1,15 @@
+/*#1*/
+let aaa = [1, 2, 3];
+console.log(aaa[0]);
+console.log(aaa[1]);
+console.log(aaa[2]);
+/*#2*/
+let arr = [1, 2, 3];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+/*#3*/
+console.log(arr[0] + arr[1] + arr[2]);
+/*#4*/
+arr = ['a', 'b', 'c' ,'d'];
+console.log(`${arr[0]}+${arr[1]}+${arr[2]}+${arr[3]} `);
