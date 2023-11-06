a63887beaf5c8f729f0838d4bd5b9d7cd15050ad
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-06 19:58:52 +0300

    71f_73f

--- src/js/71.js
+++ src/js/71.js
@@ -1,2 +1,5 @@
 let arr = ['a', 'b', 'c'];
-console.log(arr[0]= '1', arr[1] = '2', arr[2] = '3');
+arr[0]= '1';
+arr[1] ='2';
+arr[2] ='3';
+console.log(arr);
--- src/js/73.js
+++ src/js/73.js
@@ -1,6 +1,5 @@
 let arr = [1, 2, 3];
-console.log(
-    ++arr[0],
-    ++arr[1],
-    ++arr[2],
-)
+++arr[0];
+++arr[1];
+++arr[2];
+console.log(arr);
