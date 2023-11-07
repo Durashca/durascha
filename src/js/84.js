5913e13a823d0d63a859a06b9853f32843a6de4d
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-07 22:16:11 +0300

    84

---
+++ src/js/84.js
@@ -0,0 +1,24 @@
+/*#1*/
+/*
+let obj = {
+    '1a': 1,
+    'b2': 2,  //для этой не обязательны кавычки
+    'с-с': 3,
+    'd 4': 4,
+    'e5': 5  // для этой тоже
+};
+*/
+/*#2*/
+let obj = {
+    '1a': 1,
+    'b2': 2,
+    'c-c':  3,
+    'd 4': 4,
+    'e5': 5
+};
+
+console.log(obj['1a']);
+console.log(obj['b2']);
+console.log(obj['c-c']);
+console.log(obj['d 4']);
+console.log(obj['e5']);
