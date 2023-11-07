3d66dc2fe253ed735bd97fe9e5345d559e4e6771
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-07 22:57:51 +0300

    87

---
+++ src/js/87.js
@@ -0,0 +1,11 @@
+let  obj = {20: 1, x: 'ээээ',z: 'хххххх', 300: 1, 250: 34};
+console.log(obj[20]);
+console.log(obj['z']);
+console.log(obj['x']);
+console.log(obj[250]);
+console.log(obj[300]);
+
+/*
+let mas = [1, 3, 6, 9, 11];
+mas[20] = 21;
+console.log(mas);*/
