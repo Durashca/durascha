b089da35ca358e9ac99c9f0d38216d9d9e6869b1
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-08 23:07:38 +0300

    101

---
+++ src/js/101.js
@@ -0,0 +1,14 @@
+/*
+//1
+const arr = ['a', 'b', 'c'];
+arr[1] = '!';
+console.log(arr); //['a', 'b', 'c']
+//2
+const arr = ['a', 'b', 'c'];
+arr = [1, 2, 3];
+console.log(arr); // ошибка
+//3
+const arr = ['a', 'b', 'c'];
+arr = ['a', 'b', 'c'];
+console.log(arr); //ошибка
+*/
