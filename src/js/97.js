7eff8b765219c759905661d9d40b1fcc147aca69
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-08 21:35:52 +0300

    97

---
+++ src/js/97.js
@@ -0,0 +1,13 @@
+//1
+console.log( typeof {x: 1, y: 2, z: 3} ); //object
+//2
+console.log( typeof [1, 2, 3] ); //object
+//3
+let arr = [1, 2, 3];
+console.log( typeof arr ); //object
+//4
+arr = [1, 2, 3];
+console.log( typeof arr[0] );//number
+//5
+arr = ['1', '2', '3'];
+console.log( typeof arr[0] ); //string
