2d6d350e26e6a9a8c1468fd7163dc4bb3d693ad5
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-08 23:20:07 +0300

    103

---
+++ src/js/103.js
@@ -0,0 +1,16 @@
+//1
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj['x']);
+//2
+obj = {x: 1, y: 2, z: 3};
+let key = 'x';
+
+console.log(obj[key]);
+//3
+obj = {x: 1, y: 2, z: 3};
+let sum = obj['x'] + obj['y'] + obj['x'];
+
+console.log(sum);
+//4
+obj = {x: 1, y: 2, z: 3};
+console.log(Object.keys(obj).length);
