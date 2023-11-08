1a3b780dd2c013af963dc59cb09a0ed13110a4b7
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-08 20:42:31 +0300

    93

---
+++ src/js/93.js
@@ -0,0 +1,21 @@
+//#1
+let key = 'x';
+
+let obj = {
+    [key]: 1,
+    y: 2,
+    z: 3
+};
+console.log(obj);
+//2
+let key1 = 'x';
+let key2 = 'y';
+let key3 = 'z';
+
+obj = {
+    [key1]: 1,
+    [key2]: 2,
+    [key3]: 3
+};
+
+console.log(obj);
