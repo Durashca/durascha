2f6a0f6d8f0010617de129c0a4ce25dc5891c2e8
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-09 22:41:43 +0300

    112

---
+++ src/js/112.js
@@ -0,0 +1,45 @@
+//1
+let test1 = '3';
+let test2 = '3';
+
+if (test1 != test2) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//2
+test1 = '3';
+test2 = '3';
+
+if (test1 !== test2) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//3
+test1 = 3;
+test2 = '3';
+
+if (test1 != test2) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//4
+ test1 = 3;
+ test2 = '3';
+
+if (test1 !== test2) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//5
+test1 = 3;
+test2 = 2;
+
+if (test1 !== test2) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
