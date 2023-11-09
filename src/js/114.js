62c1c2389c387a9f6382f1d8b85b22042ba8ff26
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-09 23:18:56 +0300

    114

---
+++ src/js/114.js
@@ -0,0 +1,69 @@
+//1
+let num1 = -10;
+let num2 = -10;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//2
+num1 = 0;
+num2 = 0;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//3
+num1 = 0;
+num2 = 5;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//4
+ num1 = 5;
+ num2 = 5;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//5
+ num1 = -5;
+ num2 = 15;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//6
+let  num = 1;
+
+if (num == 0 || num == 1) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//7
+num = 2;
+
+if (num == 0 || num == 1) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//8
+num = 2;
+
+if (num == 0 || num == 1 || num == 2) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
