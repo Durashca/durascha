658cae7c3d7cb7f0ca1296b02c6e8a3650dfc333
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-09 22:51:35 +0300

    113

---
+++ src/js/113.js
@@ -0,0 +1,12 @@
+let num = 1;
+let num2 = 3;
+
+if (num > 0 && num < 5){
+    console.log('#1 yes')
+}
+if (num >= 10 && num <= 20){
+    console.log('#2 no')
+}
+if (num <= 1 && num2 >= 3){
+    console.log('#3 yes')
+}
