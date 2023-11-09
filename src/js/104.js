6dc542130d6c7b6294b96c83a762441a9cf69720
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-09 21:10:04 +0300

    104

---
+++ src/js/104.js
@@ -0,0 +1,20 @@
+let test = 20
+//1
+if (test > 10){
+    console.log('#1 yes');
+}
+test = 9;
+//2
+if (test < 10){
+    console.log('#2 yes');
+}
+test =20
+//3
+if(test >= 10){
+    console.log('#3 yes');
+}
+test = 9
+//4
+if (test <= 10){
+    console.log('#4 yes');
+}
