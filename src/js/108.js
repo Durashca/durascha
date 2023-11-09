cbbac8aca154ac77efaeba8d168d918be05f1148
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-09 21:52:43 +0300

    108

---
+++ src/js/108.js
@@ -0,0 +1,14 @@
+let test2 = 2
+let test1 = 1
+//1
+if (test2 > test1){
+    console.log('test2')
+}else {
+    console.log('test1')
+}
+//2
+if (test2 == test1){
+    console.log('равны')
+}else  {
+    console.log('не равны')
+}
