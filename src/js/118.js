49c435da784c149c2d797ac48787eca54eedbe31
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-10 22:32:38 +0300

    118

---
+++ src/js/118.js
@@ -0,0 +1,16 @@
+//1
+let test = true;
+
+if (test === true) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//2
+test = false;
+
+if (test === false) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
