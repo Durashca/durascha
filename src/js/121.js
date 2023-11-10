5614be66a3ecb60fa6776c257c85de1db5a0bf30
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-10 23:03:47 +0300

    121

---
+++ src/js/121.js
@@ -0,0 +1,24 @@
+//1
+let test = true;
+
+if (!test) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//2
+ test = true;
+
+if (!test) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//3
+test = true;
+
+if (!test) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
