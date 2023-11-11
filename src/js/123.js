7cfffe7d5c9b928aef9c8398dfdc47f6e090b6d9
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-11 18:51:25 +0300

    123

---
+++ src/js/123.js
@@ -0,0 +1,66 @@
+//1
+let test1 = true;
+let test2 = true;
+
+if (test1  && test2) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//2
+test1 = true;
+test2 = true;
+
+if (test1 && test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//3
+test1 = true;
+test2 = true;
+
+if (test1 && test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//4
+test1 = true;
+test2 = true;
+
+if (test1 && test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//5
+test1 = true;
+test2 = true;
+let test3 = true;
+
+if (test1 && test2 && test3) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//6
+test1 = true;
+test2 = true;
+test3 = true;
+
+if (test1 || test2 && test3) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+//7
+test1 = true;
+test2 = true;
+test3 = true;
+
+if (test1 || test2 && test3) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
