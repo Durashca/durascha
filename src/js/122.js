8f761d54ca60466068cf9a3b0018e5eb085c6aaa
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-11 18:12:42 +0300

    122

---
+++ src/js/122.js
@@ -0,0 +1,80 @@
+//1
+let test = 3;
+
+if (test) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//2
+test = 'abc';
+
+if (test) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//3
+test = '';
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//4
+test = 3 * 'abc';
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//5
+test = null;
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//6
+test = false;
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//7
+test = undefined;
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//8
+test = 0;
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---'); //это
+}
+//9
+test = '0';
+
+if (test) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
+//10
+test = -1;
+
+if (test) {
+    console.log('+++'); //это
+} else {
+    console.log('---');
+}
