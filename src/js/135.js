820dab0a9680d29e7d57a48ed0beb094150fb6ae
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-12 18:04:30 +0300

    135

---
+++ src/js/135.js
@@ -0,0 +1,61 @@
+//1
+/*
+let age = 17;
+let adult;
+
+if (age >= 18) {
+    adult = true;
+} else {
+    adult = false;
+}
+
+console.log(adult);
+*/
+
+//2
+/*
+let age = 17;
+let adult;
+
+if (age >= 18) {
+    adult = true;
+} else {
+    adult = false;
+}
+
+console.log(adult);
+*/
+
+//3
+/*
+let age = 17;
+let res;
+
+if (age >= 18) {
+    if (age <= 23) {
+        res = 'от 18 до 23';
+    } else {
+        res = 'больше 23';
+    }
+} else {
+    res = 'меньше 18';
+}
+
+console.log(res);*/
+
+//4
+let age = 19;
+let res;
+
+if (age >= 18) {
+
+    if (age <= 23) {
+        res = 'от 18 до 23';
+    } else {
+        res = 'больше 23';
+    }
+} else {
+    res = 'меньше 18';
+}
+
+console.log(res);
