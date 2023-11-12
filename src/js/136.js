8db1e5804de4c332e1b396bd449e4730736c733e
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-12 18:14:59 +0300

    136

---
+++ src/js/136.js
@@ -0,0 +1,11 @@
+let min = 19
+
+if (0 <= min && min <= 20){
+    console.log('первая треть часа')
+}else if (21 <= min && min  <= 40){
+    console.log('вторая треть часа')
+}else  if(41 <= min && min <= 60){
+    console.log('треть часа')
+}else {
+    console.log('ошибка')
+}
