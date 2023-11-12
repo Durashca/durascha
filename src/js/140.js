09a29bf83fa49d470ddae7539a426737fd7ed371
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-12 20:40:45 +0300

    140

---
+++ src/js/140.js
@@ -0,0 +1,15 @@
+//1
+let num = 14
+
+if(num % 2 === 0){
+    console.log('число четное')
+}else {
+    console.log('число не четное')
+}
+//2
+num = 15
+if (num % 3 === 0){
+    console.log('делется')
+}else {
+    console.log('не делется')
+}
