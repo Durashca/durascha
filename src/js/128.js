05167c9fcd50a907bec43f5a0d94f80b4c3df4f3
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-12 14:49:38 +0300

    128

---
+++ src/js/128.js
@@ -0,0 +1,13 @@
+let num = 10
+if (10 <= num && num <= 99) {
+    for (var i = 0; 0 < num; num--) {
+        i += num
+    }
+    console.log(i)
+} else if (num <= 9) {
+    console.log('сумма цифр однозначна')
+} else {
+    console.log('сумма цифр двухзначна')
+}
+
+
