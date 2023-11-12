696fc00f3b6a921927384299913ea4904316fccd
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-12 15:00:28 +0300

    129

---
+++ src/js/129.js
@@ -0,0 +1,32 @@
+let lang = 'ru';
+switch (lang){
+    case "ru":
+        console.log('правильно')
+        break
+    case 'en':
+        console.log('не правильно')
+        break
+    case 'анг':
+        console.log('не правильно')
+        break
+    case 'de':
+        console.log('не правильно')
+        break
+    case 'нем':
+        console.log('не правильно')
+        break
+    default:
+        console.log('язык не поддерживается');
+        break
+}
+
+/*
+if (lang == 'ru') {
+    console.log('рус');
+} else if (lang == 'en') {
+    console.log('анг');
+} else if (lang == 'de') {
+    console.log('нем');
+} else {
+    console.log('язык не поддерживается');
+}*/
