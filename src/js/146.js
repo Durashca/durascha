c2d069eb6030012285534addbecf85c390c52301
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-13 20:16:11 +0300

    146

---
+++ src/js/146.js
@@ -0,0 +1,25 @@
+//1
+let num = 0
+
+while (num < 100) {
+    ++num
+    console.log(num)
+}
+
+//2
+let num2 = 11
+
+while (num2 < 33) {
+    num2++
+    console.log(num2)
+}
+
+//3
+num = 1
+let  i = 0
+while (num * 3 < 1000){
+    num *= 3
+    i += 1
+}
+console.log('число ' + num + ',', 'кол итераций ' + i)
+
