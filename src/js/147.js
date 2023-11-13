fb7a9d6a02913921f043e4f355aff2f3a4d5e457
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-13 20:47:07 +0300

    147

---
+++ src/js/147.js
@@ -0,0 +1,20 @@
+//1
+for (let i = 1; i <= 100; i++){
+    console.log('test1 ' + i)
+}
+//2
+for (let i= 11; i <= 33; i++){
+    console.log('test2 ' + i)
+}
+//3
+for (let i = 0; i <= 100; i++){
+    console.log( 'test3 ' + i)
+}
+//4
+for (let i = 1; i < 100; i++){
+    console.log('test4 ' + i)
+}
+//5
+for (let i = 100; i >= 0; i--){
+    console.log('test5 ' + i)
+}
