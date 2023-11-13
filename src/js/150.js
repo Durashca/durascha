19cc9f9c331f7271a2ceb34f1eb1849796c70229
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-13 23:52:38 +0300

    150

---
+++ src/js/150.js
@@ -0,0 +1,27 @@
+//1
+
+let tot = 0
+
+for (let i = 2; i <= 100; i++) {
+    tot += i
+}
+console.log(tot)
+
+//2
+
+tot = 0
+
+for (let i = 1; i <= 99; i++) {
+    tot += i
+}
+console.log(tot)
+
+//3
+
+tot = 0
+
+for (let i = 1; i <= 20; i++){
+    tot += i
+}
+
+console.log(tot)
