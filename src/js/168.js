c2702c33f7e7a13be5b62e435b5c58b62b4010f4
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-16 22:16:59 +0300

    168

---
+++ src/js/168.js
@@ -0,0 +1,22 @@
+//1
+let arr = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['j', 'k', 'l'],
+];
+
+console.log(arr[3] [2], arr[1] [1], arr[2] [0], arr[0] [0])
+
+//2
+
+arr = [[1, 2], [3, 4], [5, 6]];
+let sum = 0
+
+for (let mas of arr){
+    for (let elem of mas){
+        sum += elem
+    }
+}
+
+console.log(sum)
