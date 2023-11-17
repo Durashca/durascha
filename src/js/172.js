5585ef39cfb8fa0c0f4e2a670c2da5c49be0124c
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-17 21:01:04 +0300

    172

---
+++ src/js/172.js
@@ -0,0 +1,28 @@
+//1
+
+let arr = [[1, 2, 3], [4, 5], [6]];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(arr[i] [j])
+    }
+}
+
+
+//2
+
+arr = [
+    [
+        [1, 2], [3, 4]],
+    [
+        [5, 6], [7, 8]
+    ]
+];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        for (let k  = 0; k < arr[i] [j].length; k++)
+        console.log(arr[i] [j] [k])
+
+    }
+}
