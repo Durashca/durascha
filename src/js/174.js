74b2d72a62ed71ce21dc0e266b5ba90e4f43aade
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-17 22:28:33 +0300

    174

---
+++ src/js/174.js
@@ -0,0 +1,82 @@
+//1
+
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = []
+    for (let j = 1; j <= 5; j++) {
+        arr[i].push(j);
+    }
+}
+
+console.log(arr);
+
+//2
+
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+
+    for (let j = 1; j <= 5; j++) {
+        arr[i].push(j);
+    }
+}
+
+console.log(arr);
+
+//3
+
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+
+    for (let j = 1; j <= 5; j++) {
+        arr[i].push(j);
+    }
+}
+
+console.log(arr);
+
+//4
+
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+
+    for (let j = 1; j <= 5; j++) {
+        arr[i].push(j);
+    }
+}
+
+console.log(arr);
+
+//5
+
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+
+    for (let j = 1; j <= 5; j++) {
+        arr[i].push(j);
+    }
+}
+
+console.log(arr);
+
+//6
+
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+
+    for (let j = 1; j <= 5; j++) {
+        arr[i].push(j);
+    }
+}
+
+console.log(arr);
