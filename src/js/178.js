6127b9fdd7142ab8f8205c6fcaf6d3b961c32d43
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-18 23:12:18 +0300

    178

---
+++ src/js/178.js
@@ -0,0 +1,9 @@
+//1
+
+let students = {
+    'group1': ['name11', 'name12', 'name13'],
+    'group2': ['name21', 'name22', 'name23'],
+    'group3': ['name31', 'name32', 'name33'],
+};
+
+console.log(students ['group3'] [0])
