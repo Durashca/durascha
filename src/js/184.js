c8a8bbdd7796abe0891bac6017d5579314b2bd45
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-19 20:40:16 +0300

    184

---
+++ src/js/184.js
@@ -0,0 +1,18 @@
+//1
+
+console.log(2 ** 10)
+
+//2
+
+console.log(Math.sqrt(245))
+
+//3
+
+let num = [4, 2, 5, 19, 13, 0, 10];
+let res = 0;
+
+for (let i of num) {
+    res += Math.pow(i, 3);
+}
+
+console.log(Math.sqrt(res));
