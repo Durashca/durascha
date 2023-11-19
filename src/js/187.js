ad2767f513e25e6f2b2642a7f21a66e8f52676f8
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-19 23:51:40 +0300

    187

---
+++ src/js/187.js
@@ -0,0 +1,13 @@
+//1
+console.log(Math.floor(Math.random() * (100 - 1 + 1)) + 10) //Math.random() * (max - min) + min;
+
+//2
+
+let mas = []
+
+for (let i = 1; i <= 10; i++){
+    mas.push(Math.floor(Math.random() * (100 - 1 + 1)) + 10)
+}
+
+console.log(mas)
+
