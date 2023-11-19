3c67bf15d6d846f61d51491f34b0a234effe5021
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-19 19:01:12 +0300

    182

---
+++ src/js/182.js
@@ -0,0 +1,27 @@
+//1
+
+let employees = [
+    {
+        name: 'name1',
+        salary: 300,
+        age: 28,
+    },
+    {
+        name: 'name2',
+        salary: 400,
+        age: 29,
+    },
+    {
+        name: 'name3',
+        salary: 500,
+        age: 30,
+    },
+];
+
+employees.push({
+    name: 'name4',
+    salary: 600,
+    age: 31
+})
+
+console.log(employees)
