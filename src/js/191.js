dc6bd70e6eed3feb83f9680b4747cccdc0a33a2b
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-20 21:34:10 +0300

    191

---
+++ src/js/191.js
@@ -0,0 +1,10 @@
+let str = 'https://code.mu/ru/javascript/book/prime/inbuilt/string/searching/'
+let html = '191.html'
+
+//1
+
+console.log(str.startsWith('https://'))
+
+//2
+
+console.log(html.endsWith('.html'))
