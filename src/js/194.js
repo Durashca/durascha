e7d2691f88aa84c9a0d022c00c2e943086d08122
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-20 22:46:00 +0300

    194

---
+++ src/js/194.js
@@ -0,0 +1,23 @@
+//1
+
+let mas =[1, 2, 3]
+let first =mas.shift()
+
+console.log(first)
+
+//2
+mas = [1, 2, 3]
+let last = mas.pop()
+console.log(last)
+
+//3
+mas =[1, 2, 3]
+mas.push('4', '5', '6')
+
+console.log(mas)
+
+//4
+mas =[1, 2, 3]
+mas.unshift('4', '5', '6')
+
+console.log(mas)
