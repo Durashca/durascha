f2e714e73b40be8980b67f8b5a2c834eeff0f55c
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-20 21:14:34 +0300

    190

---
+++ src/js/190.js
@@ -0,0 +1,5 @@
+let str = 'я учу javascript!'
+
+console.log(str.substring(1, 5), str.substring(5, 17))
+console.log(str.substr(1, 4), str.substr(5, 17))
+console.log(str.slice(1, 5), str.substr(5, 17))
