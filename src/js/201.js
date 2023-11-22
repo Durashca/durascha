6d645b982b9afdeda3cc993648467e3e72b48328
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-22 22:51:38 +0300

    201

---
+++ src/js/201.js
@@ -0,0 +1,20 @@
+//1
+
+function cube (num) {
+  num = num ** 3
+  console.log(num)
+}
+
+cube(2)
+
+//2
+
+function  positive (num) {
+    if (num > 1){
+        console.log('+++')
+    }else {
+        console.log('---')
+    }
+}
+
+positive(2)
