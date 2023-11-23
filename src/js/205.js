f05d1b231e32962bf31df05dd8dbfef0d39629e1
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-23 22:26:28 +0300

    205

---
+++ src/js/205.js
@@ -0,0 +1,17 @@
+//1
+function kube (num) {
+    return num ** 3
+}
+
+let res = kube(3)
+
+console.log(res)
+
+//2
+
+console.log(root(3, 4))
+
+function root (num1, num2) {
+    return Math.sqrt(num1) + Math.sqrt(num2)
+
+}
