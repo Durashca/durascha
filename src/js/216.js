3e384855a7f0c621a3ffaef3fe81c368fa216a6f
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-25 17:29:49 +0300

    216

---
+++ src/js/216.js
@@ -0,0 +1,15 @@
+//1
+function func() {
+    let num = 5;
+    return num;
+}
+
+// console.log('#1',num);  не выведет
+
+//2
+function func2() {
+    let num = 5;
+    return num;
+}
+
+console.log('#2',func2()); //5
