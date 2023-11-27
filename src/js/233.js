2706be905be573b3e313878ba83e079bd9e61a3f
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-27 21:18:39 +0300

    233

---
+++ src/js/233.js
@@ -0,0 +1,31 @@
+//1
+-
+    function func() { // Function Expression
+        console.log('!');
+    }
+//2
+- 1;
+
+function func() { //Function Declaration
+    console.log('!');
+}
+
+//3
+-1
+
+function func() { //Function Declaration
+    console.log('!');
+}
+
+//4
+1
+
+function func() { //Function Declaration
+    console.log('!');
+}
+
+//5
+-1 -
+function func() { // Function Expression
+    console.log('!');
+}
