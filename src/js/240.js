3b484b081b7b03ee7c312a825b0a8f1960ff5c00
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-27 23:30:54 +0300

    240

---
+++ src/js/240.js
@@ -0,0 +1,15 @@
+test(
+    function () {
+        return 1;
+    },
+    function () {
+        return 2;
+    },
+    function () {
+        return 3;
+    }
+)
+
+function test(func1, func2, func3) {
+    console.log(func1() + func2() + func3())
+}
