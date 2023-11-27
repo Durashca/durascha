07f36cebce58803f3f97bff389af313edad6ee40
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-27 21:34:40 +0300

    236

---
+++ src/js/236.js
@@ -0,0 +1,27 @@
+//1
+function func() { // Function Declaration
+    console.log('!');
+}
++1;
+//2
+function func() { // Function Declaration
+    console.log('!');
+} + 1;
+//3
++function func() { //  Function Expression
+    console.log('!');
+} + 1;
+//4
++
+function func() { //  Function Expression
+    console.log('!');
+} + 1;
+//5
++ 1
+function func() { // // Function Declaration
+    console.log('!');
+} + 1;
+//6
+function func() { //  Function Expression
+    console.log('!');
+} + console.log('!');
