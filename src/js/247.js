5e3b68e35eb3756d1096c7db3d2b4401d004c4aa
Durashca <144939800+Durashca@users.noreply.github.com>
2023-11-29 22:35:34 +0300

    247

---
+++ src/js/247.js
@@ -0,0 +1,21 @@
+//1
+function test(num1, num2) {
+    function func() {
+        console.log(num1 + num2); // 3
+    }
+
+    func();
+}
+
+test(1, 2);
+//2
+function test2(num1, num2) {
+    function func() {
+        console.log(num1 + num2); // 4
+    }
+
+    num1 = 2;
+    func();
+}
+
+test2(1, 2);
