2e0e9d14c47d251464e16469b16c5f11c8617886
Durashca <144939800+Durashca@users.noreply.github.com>
2023-12-13 23:56:14 +0300

    330f

--- src/js/330.js
+++ src/js/330.js
@@ -1,3 +1,3 @@
 let date1 = new Date()
 let date2 = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate(), 24)
-console.log((date2 - date1) / (1000 * 60))
+console.log(((date2 - date1) / (1000 * 60)).toFixed(0))
