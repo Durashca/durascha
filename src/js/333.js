2f9e131caa2653804dd43342169dbb5d415265ef
Durashca <144939800+Durashca@users.noreply.github.com>
2023-12-14 23:09:03 +0300

    333f

--- src/js/333.js
+++ src/js/333.js
@@ -1,5 +1,8 @@
 let date1 = '2020-11-31';
 let date2 = '2020-12-01';
 
-console.log(date1 > date2)
-console.log(date1 < date2)
+if (date1 < date2){
+    console.log( 'больше', 'date2',  )
+}else {
+    console.log( 'больше', 'date1',  )
+}
