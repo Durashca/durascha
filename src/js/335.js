53c2fe35f144203f38c137eaf48d1d114d958db1
Durashca <144939800+Durashca@users.noreply.github.com>
2023-12-14 23:49:41 +0300

    335

---
+++ src/js/335.js
@@ -0,0 +1,27 @@
+let data = '03-27'
+
+if ('03-21' <= data && data <= '04-19'){
+    console.log('Овен ')
+}else if ('04-21' <= data && data <= '05-21'){
+    console.log('Телец ')
+}else if ('05-22' <= data && data <= '06-21'){
+    console.log('Близнецы ')
+}else if ('06-22' <= data && data <= '07-22'){
+    console.log('Рак ')
+}else if ('07-23' <= data && data <= '08-21'){
+    console.log('Лев ')
+}else if ('08-22' <= data && data <= '09-23'){
+    console.log('Дева ')
+}else if ('09-24' <= data && data <= '10-23'){
+    console.log('Весы')
+}else if ('10-24' <= data && data <= '11-22'){
+    console.log('Скорпион ')
+}else if ('11-23' <= data && data <= '12-22'){
+    console.log('Стрелец')
+}else if ('12-23' <= data && data <= '01-20'){
+    console.log('Козерог ')
+}else if ('01-21' <= data && data <= '02-19'){
+    console.log('Водолей ')
+}else if ('02-20' <= data && data <= '03-20'){
+    console.log('Рыбы ')
+}
